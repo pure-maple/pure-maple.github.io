84977a2ba8ebe0cebc254ea166fe1d0ad1cb5f5e
pure-maple <pure-maple@users.noreply.github.com>
2024-12-29 12:51:03 +0000

    修复配置，提交文章 1. 修复`_config.yml`中abbrlink配置 2. 新增文章 pure-maple/Hexo-source@bef444e4486df058b4e6fb6855f1a01d1655b0cd

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/0/"];function toRandomPost(){
+var posts=["posts/4c63bf01/","posts/944687f6/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
