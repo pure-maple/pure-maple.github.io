23f09a9bec756898cde33f2efb73829865608d77
pure-maple <pure-maple@users.noreply.github.com>
2025-02-27 14:55:31 +0000

    发布文章 pure-maple/Hexo-source@ac89f986325da38cf6382f208a18475decf3c403

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4c63bf01/","posts/944687f6/"];function toRandomPost(){
+var posts=["posts/944687f6/","posts/b6d20742/","posts/d05e0771/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
