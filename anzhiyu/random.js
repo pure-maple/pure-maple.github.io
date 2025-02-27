5d245a622db278736b385cf78956e39b6b44d6a6
pure-maple <pure-maple@users.noreply.github.com>
2025-02-27 15:01:28 +0000

    删除文章 pure-maple/Hexo-source@b67d40e127b960468d4875c2dc926606df5b8f13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/944687f6/","posts/b6d20742/","posts/d05e0771/"];function toRandomPost(){
+var posts=["posts/b6d20742/","posts/d05e0771/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
